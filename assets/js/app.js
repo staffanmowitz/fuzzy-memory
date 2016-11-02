cbb19270e916eb98de90b70a39ea357ff5a59e7d
Staffan Mowitz <staffan.mowitz@gmail.com>
2016-11-02 20:50:19 +0100

    Added function to shuffle the cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Give each card a random specific value = the picture on the front of the card
-var cards = [
+const cards = [
   'img-1',
   'img-1',
   'img-2',
@@ -20,14 +20,36 @@ var cards = [
   'img-8'
 ];
 
+const newGame = document.querySelector('a');
+
+// The Fisher-Yates shuffle
+function shuffleCards (array) {
+  let i = 0;
+  let j = 0;
+  let temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 function dealCards() {
+  shuffleCards(cards);
   document.querySelector('div.cards').innerHTML = "";
   cards.forEach(function(i) {
-    var card = document.createElement('div');
+    let card = document.createElement('div');
     card.className = 'card';
-    //card.innerHTML = i;
+    card.innerHTML = i;
     document.querySelector('div.cards').appendChild(card);
   });
 }
 
 dealCards();
+
+newGame.addEventListener('click', function(event){
+  event.preventDefault();
+  dealCards();
+});
