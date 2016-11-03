c8d695825b96fc10a2ec403335497bc070572b75
Staffan Mowitz <staffan.mowitz@gmail.com>
2016-11-03 21:59:01 +0100

    Rewrote script to handle imgs instead of divs

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,22 @@
 
 // Give each card a random specific value = the picture on the front of the card
 const cards = [
-  'img-1',
-  'img-1',
-  'img-2',
-  'img-2',
-  'img-3',
-  'img-3',
-  'img-4',
-  'img-4',
-  'img-5',
-  'img-5',
-  'img-6',
-  'img-6',
-  'img-7',
-  'img-7',
-  'img-8',
-  'img-8'
+  ['card-1', 'img-1.png'],
+  ['card-2', 'img-1.png'],
+  ['card-3', 'img-2.png'],
+  ['card-4', 'img-2.png'],
+  ['card-5', 'img-3.png'],
+  ['card-6', 'img-3.png'],
+  ['card-7', 'img-4.png'],
+  ['card-8', 'img-4.png'],
+  ['card-9', 'img-5.png'],
+  ['card-10', 'img-5.png'],
+  ['card-11', 'img-6.png'],
+  ['card-12', 'img-6.png'],
+  ['card-13', 'img-7.png'],
+  ['card-14', 'img-7.png'],
+  ['card-15', 'img-8.png'],
+  ['card-16', 'img-8.png']
 ];
 
 // Shuffle the content of the cards-array (Fisher-Yates shuffle)
@@ -33,31 +33,27 @@ function shuffleCards (array) {
   }
 }
 
+const dealedCards = document.querySelectorAll('.card');
+
 function dealCards () {
   shuffleCards(cards);
-  document.querySelector('div.cards').innerHTML = '';
-  cards.forEach(function (i) {
-    let cardDiv = document.createElement('div');
-    cardDiv.className = 'card';
-    cardDiv.setAttribute('data-image', i + '.png');
-    cardDiv.innerHTML = '<img src="assets/img/back.png">';
-    document.querySelector('div.cards').appendChild(cardDiv);
-  });
+  for (let i = 0; i < dealedCards.length; i++) {
+    let dealedCard = dealedCards[i];
+    dealedCard.setAttribute('src', 'assets/img/back.png');
+    dealedCard.setAttribute('data-image', 'assets/img/' + cards[i][1]);
+  }
 }
 
 dealCards();
 
-const newGame = document.querySelector('button');
-newGame.addEventListener('click', dealCards, false);
+const replay = document.querySelector('button');
+replay.addEventListener('click', dealCards, false);
+
+for (let i = 0; i < dealedCards.length; i++) {
+  dealedCards[i].addEventListener('click', flipCard, false);
+}
 
 function flipCard () {
   let cardImg = this.getAttribute('data-image');
-  this.innerHTML = '<img src="assets/img/' + cardImg + '">';
-}
-
-const card = document.querySelectorAll('.card');
-console.log(card);
-
-for (let i = 0; i < card.length; i++) {
-  card[i].addEventListener('click', flipCard, false);
+  this.setAttribute('src', cardImg);
 }
